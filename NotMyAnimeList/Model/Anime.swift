31b106917871587910da61a96824a44013378f3d
Isaac Delgado <danielidc.dz@gmail.com>
2017-08-27 12:38:04 -0700

    - Created project and .gitignore

---
+++ NotMyAnimeList/Model/Anime.swift
@@ -0,0 +1,16 @@
+//
+//  Anime.swift
+//  NotMyAnimeList
+//
+//  Created by Isaac Delgado on 8/27/17.
+//  Copyright © 2017 Isaac Delgado. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+import ObjectMapper
+
+class Anime: Object {
+
+    
+}
